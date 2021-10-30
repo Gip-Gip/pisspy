ff52c84f0edfeffcf02632d0ba0f5e59ca5daeae
Charles Thompson <chmithbiz@gmail.com>
2021-10-30 17:30:25 -0400

    Completed basic qr label generation

--- src/pisspy/pissqrgen.py
+++ src/pisspy/pissqrgen.py
@@ -17,12 +17,13 @@
 # getIdNum - get a fresh id number                                             #
 # formatIdNum - format the given id number to be printed                       #
 # makeQrCode - make a qr code image given an id number                         #
+# makeLabel - make a label given an id number                                  #
 #                                                                              #
 #$@$$@$$@$$@$$@$$@$$@$$@$$@$$@$$@$$@$$@$$@$$@$$@$$@$$@$$@$$@$$@$$@$$@$$@$$@$$@$#
 #==============================================================================#
 
 import pyqrcode, math;
-from PIL import Image, ImageOps;
+from PIL import Image, ImageOps, ImageFont, ImageDraw;
 from io import BytesIO;
 
 
@@ -84,17 +85,20 @@ def formatIdNum(idNum):
 #        variable to define the width and the height
 
 def makeQrCode(idNum, size):
+    # QR code quiet zone, minimum size being 4
+    QUIET_ZONE = 4;
     # make the qr code scale equal to the size over the width and height of
-    # the qr code in modules (29)
-    modScale = math.floor(size / 29)
+    # the qr code in modules
+    modScale = math.floor(size / (21 + (2 * QUIET_ZONE)))
 
     # Create the qr code
     qr = pyqrcode.create(idNum, error='H', version=1);
 
     # Convert the qr code to an intermediate XBM format
+    # Don't ask me why pyqrcode can't output PIL images!
     # Set the quiet zone to 1+ the default so we can just crop the image to the
     # right width+height
-    xbm = qr.xbm(scale = modScale, quiet_zone=5);
+    xbm = qr.xbm(scale = modScale, quiet_zone = QUIET_ZONE + 1);
 
     # Convert this xbm to a PIL image
     # Seems like a bunch of hoopla to me!
@@ -122,4 +126,56 @@ def makeQrCode(idNum, size):
     img = img.crop((cutSize, cutSize, size + cutSize, size + cutSize));
 
     # And return!
+    return img;
+
+
+
+# makeLabel - make a PIL label from an id number
+#
+# ARGUMENTS:
+# idNum - the id number to make a label from
+# width - the width of the label
+# height - the height of the label
+# dpi - the dpi setting we're using
+
+def makeLabel(idNum, width, height, dpi):
+    # We can't even specify dpi, idk why the hell we use PT as our units
+    # 32pt = 19px*27px, 64pt = 38px*54px, 128pt = 77px*107px
+    # 1*1pt = 0.6015625*0.8359375px approximately
+    PT_WIDTH = 0.6015625;
+    PT_HEIGHT = 0.8359375;
+    FONT_FILE = "cour.ttf"; # Set the font file to Courier New
+
+    # Convert the id number to a formatted string
+    idNumStr = formatIdNum(idNum);
+    # Have the size of the font tied to the width of the label. Why?
+    # Easier to program, that simple
+    fontSize = width / (PT_WIDTH * len(idNumStr));
+
+    # Generate a qr code with the remaining space, by setting the size to either
+    # the remaining vertical space(height - the vertical size of the text), or
+    # the width of the label, whichever is smaller
+    qrSize = math.floor(min(height - (fontSize * PT_HEIGHT), width));
+    qrImg = makeQrCode(idNum, qrSize);
+    # Calculate the XY of the qr code in the label
+    # The X is calculated by getting the remainder of the horizontal free space
+    # (width - qrSize) and dividing it by 2
+    # The Y is equal to the height of out font
+    qrXY = (math.floor((width - qrSize) / 2), math.floor(fontSize * PT_HEIGHT));
+
+    # Load the font
+    """MAKE SURE YOU HAVE THE COURIER NEW FONT INSTALLED"""
+    font = ImageFont.truetype(FONT_FILE, size = math.floor((fontSize)));
+
+    # Create a blank image for our label
+    img = Image.new("RGB", (width, height), color="white");
+    # And a draw object so we can draw to our image
+    draw = ImageDraw.Draw(img);
+
+    # Draw the text at 0, 0
+    draw.text((0, 0), idNumStr, font = font, spacing = 0, fill = "black");
+    # Paste the qr code at the qr code position
+    img.paste(qrImg, box = qrXY);
+
+    # Lastily, return the image!
     return img;
